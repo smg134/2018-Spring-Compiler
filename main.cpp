3fc8e44204f900b16e7e531a7ac81163cf1c5eb7
Sean Gaffney <smg134@zips.uakron.edu>
2018-02-16 17:12:59 -0500

    Added test file

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+#include "File.h"
+#include "Lexer.h"
+#include <iostream>
+
+int main()
+{
+	File input("testFile.txt");
+	SymbolTable syms;
+	Lexer lex(syms, input);
+	while (Token tok = lex())
+		std::cout << tok << '\n';
+}
