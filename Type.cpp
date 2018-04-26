c2f118d4e7604083c8073106f98cf2d7bcf4da49
Sean Gaffney <smg134@zips.uakron.edu>
2018-04-26 12:11:04 -0400

    Added much

--- Type.cpp
+++ Type.cpp
@@ -1,3 +1,81 @@
 #pragma once
 #include "stdafx.h"
 #include "Type.h"
+
+bool Type::isReferenceTo(const Type* t) {
+	if (const ReferenceType* r = dynamic_cast<const ReferenceType*>(this)) {
+		return areSame(r->getObjectType(), t);
+	}
+	return false;
+}
+
+bool Type::isPointerTo(const Type* t) {
+	if (const PointerType* p = dynamic_cast<const PointerType*>(this)) {
+		return areSame(p->getElementType(), t);
+	}
+	return false;
+}
+
+bool Type::isArithmetic() const {
+	return kind == int_kind || kind == float_kind;
+}
+
+bool Type::isNumeric() const {
+	switch (kind) {
+	case bool_kind:
+	case char_kind:
+	case int_kind:
+	case float_kind:
+		return true;
+	default:
+		return false;
+	}
+}
+
+Type* Type::getObjectType() const {
+	if (const ReferenceType* r = dynamic_cast<const ReferenceType*>(this)) {
+		return r->getObjectType();
+	}
+	return const_cast<Type*>(this);
+}
+
+bool areSamePointers(const PointerType* t1, const PointerType* t2) {
+	return areSame(t1->getElementType(), t2->getElementType());
+}
+
+bool areSameReferences(const ReferenceType* t1, const ReferenceType* t2) {
+	return areSame(t1->getObjectType(), t2->getObjectType());
+}
+
+bool areSameFunctions(const FunctionType* t1, const FunctionType* t2) {
+	auto cmp = [](const Type* a, const Type* b) {
+		return areSame(a, b);
+	};
+	const TypeList& p1 = t1->getParamTypes();
+	const TypeList& p2 = t2->getParamTypes();
+	return std::equal(p1.begin(), p1.end(), p2.begin(), p2.end(), cmp);
+}
+
+bool areSame(const Type* t1, const Type* t2) {
+	if (t1 == t2) {
+		return true;
+	}
+
+	if (t1->getKind() != t2->getKind()) {
+		return false;
+	}
+
+	switch (t1->getKind()) {
+	case Type::bool_kind:
+	case Type::char_kind:
+	case Type::int_kind:
+	case Type::float_kind:
+		return true;
+	case Type::pointer_kind:
+		return areSamePointers(static_cast<const PointerType*>(t1), static_cast<const PointerType*>(t2));
+	case Type::reference_kind:
+		return areSameReferences(static_cast<const ReferenceType*>(t1), static_cast<const ReferenceType*>(t2));
+	case Type::function_kind:
+		return areSameFunctions(static_cast<const FunctionType*>(t1), static_cast<const FunctionType*>(t2));
+	}
+}
--- Type.h
+++ Type.h
@@ -1,14 +1,97 @@
 #pragma once
+#include <vector>
 
-class Type {};
-class BooleanType {};
-class IntegerType {};
-
-class ReferenceType {
+class Type {
 public:
-	ReferenceType(Type* t)
-		: object(t) {}
+	enum Kind {
+		bool_kind,
+		char_kind,
+		int_kind,
+		float_kind,
+		pointer_kind,
+		reference_kind,
+		function_kind
+	};
+
+protected:
+	Type(Kind k)
+		: kind(k) {}
+
+public:
+	virtual ~Type() = default;
+
+	Kind getKind() const { return kind; }
+
+	bool isInt() const { return kind == int_kind; }
+	bool isBool() const { return kind == bool_kind; }
+	bool isFloat() const { return kind == float_kind; }
+	bool isChar() const { return kind == char_kind; }
+	bool isReference() const { return kind == reference_kind; }
+	bool isReferenceTo(const Type* t);
+	bool isPointer() const { return kind == pointer_kind; }
+	bool isPointerTo(const Type* t);
+	bool isFunction() const { return kind == function_kind; }
+	bool isObject() const { return !isReference(); }
+	bool isArithmetic() const;
+	bool isNumeric() const;
+	bool isScalar() const { return true; }
+	
+	Type* getObjectType() const;
 
 private:
-	Type* object;
+	Kind kind;
 };
+
+using TypeList = std::vector<Type*>;
+
+struct BoolType : Type {
+	BoolType()
+		: Type(bool_kind) {}
+};
+
+struct CharType : Type {
+	CharType()
+		: Type(char_kind) {}
+};
+
+struct IntType : Type {
+	IntType()
+		: Type(int_kind) {}
+};
+
+struct FloatType : Type {
+	FloatType()
+		: Type(float_kind) {}
+};
+
+struct PointerType : Type {
+	PointerType()
+		: Type(pointer_kind) {}
+
+	Type* getElementType() const { return element; }
+
+	Type* element;
+};
+
+struct ReferenceType : Type {
+	ReferenceType()
+		: Type(reference_kind) {}
+
+	Type* getObjectType() const { return element; }
+
+	Type* element;
+};
+
+struct FunctionType : Type {
+	FunctionType(const TypeList& p, Type* r)
+		: Type(function_kind), params(p), returnType(r) {}
+
+	const TypeList& getParamTypes() const { return params; }
+	TypeList& getParamTypes() { return params; }
+	Type* getReturnType() const { return returnType; }
+
+	TypeList params;
+	Type* returnType;
+};
+
+bool areSame(const Type* t1, const Type* t2);
